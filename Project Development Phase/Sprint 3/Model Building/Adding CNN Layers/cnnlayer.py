61ade1512d8d2560aced069f39179baa267dfc6b
Padma Priya T R <85836583+Technopile-PadmaPriyaTR@users.noreply.github.com>
2022-10-26 20:11:47 +0530

    Create cnnlayer.py

---
+++ Project Development Phase/Sprint 3/Model Building/Adding CNN Layers/cnnlayer.py
@@ -0,0 +1,5 @@
+model.add(Convolution2D(32, (3, 3), input_shape=(80, 80, 1), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model1.add(Flatten())
