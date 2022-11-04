631035ec5bc6af1c6b9ff5df5bdc84e398272c40
Padma Priya T R <85836583+Technopile-PadmaPriyaTR@users.noreply.github.com>
2022-11-04 13:43:01 +0530

    Update Dense.py

--- Project Development Phase/Sprint 3/Model Building/Adding Dense Layers/Dense.py
+++ Project Development Phase/Sprint 3/Model Building/Adding Dense Layers/Dense.py
@@ -1,3 +1,4 @@
-model.add(Dense(units=512 , activation='relu'))
-model.add(Dense(units=6, activation='softmax'))
-model.summary()
+model.add(Dense(units=128,activation = 'relu'))
+model.add(Dense(units = 64, activation = 'relu'))
+model.add(Dense(units = 32, activation = 'relu'))
+model.add(Dense(units = 6, activation = 'softmax'))
