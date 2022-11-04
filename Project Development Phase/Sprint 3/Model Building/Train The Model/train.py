c2202941fe3e40e4b21210dddba8c6a6df4b11b8
Padma Priya T R <85836583+Technopile-PadmaPriyaTR@users.noreply.github.com>
2022-11-04 13:44:48 +0530

    Update train.py

--- Project Development Phase/Sprint 3/Model Building/Train The Model/train.py
+++ Project Development Phase/Sprint 3/Model Building/Train The Model/train.py
@@ -3,10 +3,13 @@ checkpoint = ModelCheckpoint(r'D:\IBM Project\gesture.h5',
 
 earlystop = EarlyStopping(monitor = 'val_loss', patience=7, verbose= 3, restore_best_weights=True)
 
-learning_rate = ReduceLROnPlateau(monitor= 'val_loss', patience=3, verbose= 3, )
+learning_rate = ReduceLROnPlateau(monitor= 'val_loss', patience=7, verbose= 3, )
 
 callbacks=[checkpoint,earlystop,learning_rate]
 
-model.fit(train_data,
-          validation_data=test_data,
-          epochs=25,callbacks=callbacks,validation_steps=len(test_data)//8)
+model.fit_generator(train_gen,
+                    epochs=25,
+                    steps_per_epoch=18000//32,
+                    validation_data=test_gen,
+                    callbacks=callbacks,
+                    verbose = 1,validation_steps=3600//32)
